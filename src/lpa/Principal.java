f01f3c2357780e33e24dafcc0a3340558ce61ad0
gi.machado <gioh.machado474@gmail.com>
2024-03-06 10:56:41 -0300

    Primeira versao do Ola Mundo

---
+++ src/lpa/Principal.java
@@ -0,0 +1,11 @@
+package lpa;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         System.out.println("Olá, SESI SENAI");
+        
+	}
+
+}
